38b5cf0682852e3d188614593d873e3a24517401
shavavo <33111844+shavavo@users.noreply.github.com>
2017-10-29 04:14:44 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import nltk
 from google.cloud import translate
+import os
+import random
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
 
-import os
+
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/davidcheng/Desktop/Parse/auth.json"
 
 def translate_text(target, text):
@@ -13,12 +15,25 @@ def translate_text(target, text):
     result = translate_client.translate(text, target_language=target)
     return result['translatedText']
 
+def random_chance(diff):
+    x = (80*diff/9) + (100/9);
+
+    if random.randint(0,99) <=  x :
+        return True
+    else:
+        return False
+
+
 paragraph = "Terry didn't consider himself particularly unusual. Sure, he spent his teenage years as a willing and sometimes absurdly cheerful social outcast, upon adulthood immediately transitioned to playing side-kick to a magic-savvy private investigator, accidentally became the confidant of an apparently ageless time-traveler, and just recently declared war on a corporation widely recognized as one of the top ten charitable organizations in the world, but he figured most people had a few weird phases in their lives."
 
 # N, V, or A
-type = "J"
+type = "N"
 
 language = "zh"
+
+# Difficulty 1-10 20% - 100%
+difficulty = 10;
+
 
 # Parses words into vector
 words = nltk.word_tokenize(paragraph);
@@ -31,15 +46,16 @@ translated = []
 
 # Iterates through part of speech and saves locations of nouns
 for idx, i in enumerate(partOfSpeech):
-    if i[1][:2] == "NN" and type == "N" :
-        toTranslate.append(i[0])
-        translated.append(translate_text(language, i[0]))
-    if i[1][:2] == "JJ" and type == "J":
-        toTranslate.append(i[0])
-        translated.append(translate_text(language, i[0]))
-    if i[1][:1] == "V" and type == "V":
-        toTranslate.append(i[0])
-        translated.append(translate_text(language, i[0]))
+    if random_chance(difficulty):
+        if i[1][:2] == "NN" and type == "N" :
+            toTranslate.append(i[0])
+            translated.append(translate_text(language, i[0]))
+        if i[1][:2] == "JJ" and type == "J":
+            toTranslate.append(i[0])
+            translated.append(translate_text(language, i[0]))
+        if i[1][:1] == "V" and type == "V":
+            toTranslate.append(i[0])
+            translated.append(translate_text(language, i[0]))
 
 css = ""
 
@@ -48,6 +64,6 @@ for i in range( len(toTranslate)-1 ):
     paragraph = paragraph[0:location] + "<code id='f"+ str(i) +"'></code>" + paragraph[location + len(toTranslate[i]):]
     css += "#f" + str(i) + ":after{content: '" + translated[i] + "';}\n#f" + str(i) + ":hover:after{content: '" + toTranslate[i] + "';}\n\n"
 
-print paragraph
-print css
-
+#print paragraph
+#print css
+print len(toTranslate)
